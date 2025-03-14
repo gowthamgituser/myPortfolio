232580b4e584e97118b4118801785fae5ee26731
Gowtham <gowthamrajendran1996@gmail.com>
2025-03-14 12:47:18 +0530

    Rename profile name

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,9 +11,9 @@ const seo = {
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Gowtham Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "",
   },
 };
 
@@ -26,8 +26,8 @@ const greeting = {
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
     "https://drive.google.com/file/d/1UEpSStQAGL8yLlv4_i4A7R9VcVgdADN8/view?usp=sharing",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
-  githubProfile: "https://github.com/ashutosh1919",
+  portfolio_repository: "",
+  githubProfile: "",
 };
 
 const socialMediaLinks = [
